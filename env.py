8fd557b838a335ce873b979b5048f8c02daf619f
Perparim <perparimshabani.97@gmail.com>
2025-12-05 18:15:48 +0100

    deployment settings

---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_9opEqgIcY0XS@ep-hidden-queen-agrhikxm.c-2.eu-central-1.aws.neon.tech/neon_dash_snack_126722"
+)
